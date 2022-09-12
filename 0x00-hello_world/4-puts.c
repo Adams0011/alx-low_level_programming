8630e2b5b095a4514dcd5137f2bd217362a787c3
Adams0011 <adamsmithoodo@gmail.com>
2022-09-12 15:09:59 -0700

    Adding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** main - Entry point
+ * print programming is like building a multilinqual puzzle to the screen
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{     
+	puts("\"programming is like building a multilingual puzzle");
+        
+         return (0);
+}	 
