8e248c51b056e300bd603d5d3f12b7dff60cc605
Adams0011 <adamsmithoodo@gmail.com>
2022-09-11 20:47:55 -0700

    Adding

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	char number, alphabet;
 
-	for (number = '0' ; number >= '9' ; number++)
+	for (number = '0' ; number <= '9' ; number++)
 	{
 		putchar(number);
 	}
-	for (alphabet = 'a' ; alphabet >= 'f' ; alphabet++)
+	for (alphabet = 'a' ; alphabet <= 'f' ; alphabet++)
 	{
 		putchar(alphabet);
 	}
