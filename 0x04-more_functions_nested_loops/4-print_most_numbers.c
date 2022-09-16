70c3fde54048f21736728ac7b94de7826eec323a
Adams0011 <adamsmithoodo@gmail.com>
2022-09-16 08:57:27 -0700

    Adding

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_most_numbers - main
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if ((1 == '2') || (i == '4'))
+		{
+			continue;
+			i++;
+		}
+		else
+		{
+		_putchar(i);
+		}
+
+	}
+	_putchar('\n');
+}
