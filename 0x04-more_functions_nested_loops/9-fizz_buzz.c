48b6fe9454be53523dd88167fdc058c283cfcf97
Adams0011 <adamsmithoodo@gmail.com>
2022-09-16 17:22:01 -0700

    Adding

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+*main - Entry point
+*print numbers 1 to 100 with conditions
+*Return: 0 once successful
+*/
+int main(void)
+{
+	int a;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char FB[] = "FizzBuzz";
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a == 100)
+			printf("%s", b);
+		else if ((a % 5 == 0) && (a % 3 == 0))
+		{
+			printf("%s ", fb);
+		}
+		else if ((a % 5) == 0)
+		{
+			printf("%s ", b);
+		}
+		else if ((a % 3) == 0)
+		{
+			printf("%s ", f);
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf('\n');
+	return (0);
+}
+
