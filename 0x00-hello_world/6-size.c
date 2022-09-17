c0f7b8048edda86d5a1c9db30b59a930390c131b
Adams0011 <adamsmithoodo@gmail.com>
2022-09-17 05:11:29 -0700

    Adding

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,17 @@
  */
 int main(void)
 {
+	char chartype;
+	int inttype;
+	long int a;
+	long long int b;
+	float floattype;
+
 	printf("size of char: %d byte(s)\n", sizeof(char));
 	print("size of int: %d byte(s)\n", sizeof(int));
 	printf("size of long int: %d byte(s)\n", sizeof(long int));
 	printf("size of long long int: %d byte(s)\n", sizeof(long long int));
 	printf("size of float: %d bytes(s)\n", sizeof(float));
+
 	return (0);
 }
