b2660c44ff1f0e07d64fd24827f9de0fa6c1bf66
Adams0011 <adamsmithoodo@gmail.com>
2022-09-23 16:41:08 -0700

    Adding

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - function that coverts lower case characters to uppercase
+ * @c: the pointer variable to the character strings
+ * Return: uppercase characters
+ */
+char *string_toupper(char *c)
+{
+	int f;
+
+	for (f = 0; c[f] != '\0'; f++)
+	{
+		if (c[f] >= 'a' && c[f] <= 'z')
+		{
+			c[f] = c[f] = 32;
+		}
+	}
+	return (c);
+}
+
