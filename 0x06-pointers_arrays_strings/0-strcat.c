2c2211d13a210157e475b042a410fa73c04e5441
Adams0011 <adamsmithoodo@gmail.com>
2022-09-22 17:15:23 -0700

    Adding

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,20 +10,16 @@ char *_strcat(char *dest, char *src)
 	int c = 0;
 	int d = 0;
 
-	/* overwrite the null byte at the end of the dest */
 		while (dest[c] != '\0')
 		{
 			c++;
 		}
-		/* copies the byte from string src*/
 		while (src[d] != '\0')
 		{
 			dest[c] = src[d];
 			c++;
 			d++;
 		}
-		/* adding the terminating null byte */
 		dest[c] = '\0';
 		return (dest);
 }
-
