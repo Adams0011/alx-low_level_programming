e1aab960d0d3ea5c80b2975e73cfa4995d39a39b
Adams0011 <adamsmithoodo@gmail.com>
2022-09-25 06:36:04 -0700

    Adding

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - a function which capitalize all words of a string
+ * @c: pointer variable that holds the array of characters in the string
+ * Return: caps on first letter of a seperator
+ */
+char *cap_string(char *c)
+{
+	int a, m;
+	int cap = 32;
+	int seprt[] = {'\n', '\t', ',', ';', '.', '?', '"', '(', ')', '{', '}', ' '};
+
+	for (a = 0; c[a] != '\0'; a++)
+	{
+		if (c[a] >= 'a' && c[a] <= z)
+		{
+			c[a] = c[a] - cap;
+		}
+
+		cap = 0;
+
+		for (m = 0; m <= 12; m++)
+		{
+			if (c[a] == seprt[m])
+			{
+				m = 12;
+				cap = 32;
+			}
+		}
+	}
+	return (c);
+}
