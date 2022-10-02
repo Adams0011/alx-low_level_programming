bce6d62be81ffcbfaa40f46f7a8d6b61247b31e3
Adams0011 <adamsmithoodo@gmail.com>
2022-10-01 23:35:18 -0700

    Adding

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr - the function that finds a substring in a string
+ * @haystack: the string with all goodies
+ * @needle: the substring to be found
+ * Return: the matching substring
+ * Else, return null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char srch;
+	char strt;
+
+	while (*haystack != '\0')
+	{
+		strt = *haystack;
+		srch = *needle
+		while (*needle != '\0' && *srch != '\0' && *srch == *haystack)
+		{
+			haystack++;
+			srch++;
+		}
+		if (*srch == '\0')
+			return (strt);
+		haystack = strt + 1;
+	}
+	return (0);
+}
+
